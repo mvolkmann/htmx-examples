c4648bfb2ebf222a02baeee9a023c92dcfb0879c
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-01-16 14:51:54 -0600

    progress

--- progress-bar/src/index.tsx
+++ progress-bar/src/index.tsx
@@ -23,12 +23,14 @@ let percentComplete = 0;
 
 function ProgressBar() {
   const attributes: {[key: string]: string} = {};
-  console.log('index.tsx : percentComplete =', percentComplete);
   if (percentComplete < 100) {
     attributes['hx-get'] = '/progress';
-    attributes['hx-trigger'] = 'load';
+    attributes['hx-trigger'] = 'load delay:1s, reset';
     attributes['hx-swap'] = 'outerHTML';
   }
+
+  // This works, but value changes cannot be animated.
+  /*
   return (
     <progress
       id="percent-complete"
@@ -37,6 +39,18 @@ function ProgressBar() {
       {...attributes}
     />
   );
+  */
+  return (
+    <div
+      class="progress-container"
+      {...attributes}
+      role="progressbar"
+      aria-valuenow={percentComplete}
+    >
+      <div class="progress-text">{percentComplete.toFixed(1)}%</div>
+      <div class="progress-bar" style={`width: ${percentComplete}%`} />
+    </div>
+  );
 }
 
 app.get('/', () => {
@@ -44,13 +58,15 @@ app.get('/', () => {
     <BaseHtml>
       <h1>Progress Bar</h1>
       <ProgressBar />
+      <button hx-on="percentComplete = 0; this.dispatchEvent('reset')">
+        Reset
+      </button>
     </BaseHtml>
   );
 });
 
 app.get('/progress', () => {
-  Bun.sleepSync(1000); // enables testing hx-indicator spinner
-  const delta = Math.random() * 10;
+  const delta = Math.random() * 20;
   percentComplete = Math.min(100, percentComplete + delta);
   return <ProgressBar />;
 });
