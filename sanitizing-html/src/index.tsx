3c418a16c31c967df9a12cbff30a2f23189cae01
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-01-17 16:08:05 -0600

    progress

--- sanitizing-html/src/index.tsx
+++ sanitizing-html/src/index.tsx
@@ -13,8 +13,23 @@ const BaseHtml: FC = ({children}) => (
   <html>
     <head>
       <title>Sanitizing HTML</title>
+      {/* This CSP says:
+          - By default, all content must come from this domain.
+          - An exception is made for images which can come from any https URL.
+          - The child-src directive defines the valid sources for
+            web workers and nested browsing contexts
+            loaded using elements such as <frame> and <iframe>.
+            No sources are valid for these.
+       */}
+      {/* <meta
+        http-equiv="Content-Security-Policy"
+        content="default-src 'self'; img-src https://*; child-src 'none';"
+      /> */}
+      {/* <meta name="htmx-config" content='{"allowScriptTags": false}' /> */}
       <link rel="stylesheet" href="/styles.css" />
-      <script src="https://unpkg.com/htmx.org@1.9.9"></script>
+      {/* The CSP above blocks getting scripts from other domains,
+          so this project includes a local copy of htmx.min.js. */}
+      <script src="htmx.min.js"></script>
     </head>
     <body>{children}</body>
   </html>
@@ -34,10 +49,12 @@ app.get('/', (c: Context) => {
 });
 
 app.post('/render', async (c: Context) => {
+  // Bun.sleepSync(5000); // simulates long-running query
   const data = await c.req.formData();
   const markup = data.get('markup');
-  console.log('index.tsx /render: markup =', markup);
   // const markup = '<p>before</p><script>alert("pwned")</script><p>after</p>';
+  // console.log('index.tsx /render: markup =', markup);
+  // return c.html(markup);
   return c.html(sanitizeHtml(markup));
 });
 
