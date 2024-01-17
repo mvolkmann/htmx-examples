4fa46e55f2b9fa0b206921a87652c144322d41fa
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-01-17 16:29:55 -0600

    progress

--- sanitizing-html/src/index.tsx
+++ sanitizing-html/src/index.tsx
@@ -26,6 +26,9 @@ const BaseHtml: FC = ({children}) => (
         content="default-src 'self'; img-src https://*; child-src 'none';"
       /> */}
       {/* <meta name="htmx-config" content='{"allowScriptTags": false}' /> */}
+      {/* The request timeout can be specified on individual requests
+          with the `hx-request` attribute. */}
+      {/* <meta name="htmx-config" content='{"timeout": 2000}' /> */}
       <link rel="stylesheet" href="/styles.css" />
       {/* The CSP above blocks getting scripts from other domains,
           so this project includes a local copy of htmx.min.js. */}
@@ -39,17 +42,25 @@ app.get('/', (c: Context) => {
   return c.html(
     <BaseHtml>
       <h1>Sanitizing HTML</h1>
-      <form hx-post="/render" hx-target="#result">
+      {/* The timeout for all requests can be specified
+          in a meta tag for htmx-config. */}
+      <form hx-post="/render" hx-target="#result" hx-request='\"timeout\":2000'>
         <input name="markup" type="text" />
-        <div id="result" />
+        <div
+          id="result"
+          hx-get="/timeout"
+          hx-trigger="htmx:timeout from:form"
+        />
         <button>Submit</button>
       </form>
     </BaseHtml>
   );
 });
 
+app.get('/timeout', (c: Context) => c.text('The request timed out.'));
+
 app.post('/render', async (c: Context) => {
-  // Bun.sleepSync(5000); // simulates long-running query
+  Bun.sleepSync(5000); // simulates long-running query
   const data = await c.req.formData();
   const markup = data.get('markup');
   // const markup = '<p>before</p><script>alert("pwned")</script><p>after</p>';
