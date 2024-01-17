fdf43bf2bc8fc6ae9cb56f964f3738729ebc0678
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-01-17 14:59:23 -0600

    progress

--- sanitizing-html/src/index.tsx
+++ sanitizing-html/src/index.tsx
@@ -1,17 +1,18 @@
-import { Hono } from "hono";
-import type { Context } from "hono";
-import { serveStatic } from "hono/bun";
-import type { FC } from "hono/jsx";
+import {Hono} from 'hono';
+import type {Context} from 'hono';
+import {serveStatic} from 'hono/bun';
+import type {FC} from 'hono/jsx';
+import sanitizeHtml from 'sanitize-html';
 
 const app = new Hono();
 
 // This serves static files from the public directory.
-app.use("/*", serveStatic({ root: "./public" }));
+app.use('/*', serveStatic({root: './public'}));
 
-const BaseHtml: FC = ({ children }) => (
+const BaseHtml: FC = ({children}) => (
   <html>
     <head>
-      <title>Polling</title>
+      <title>Sanitizing HTML</title>
       <link rel="stylesheet" href="/styles.css" />
       <script src="https://unpkg.com/htmx.org@1.9.9"></script>
     </head>
@@ -19,38 +20,25 @@ const BaseHtml: FC = ({ children }) => (
   </html>
 );
 
-app.get("/", (c: Context) => {
+app.get('/', (c: Context) => {
   return c.html(
     <BaseHtml>
-      <h1>Fixed Rate Polling</h1>
-      <h2 hx-get="/score" hx-trigger="load, every 5s" />
+      <h1>Sanitizing HTML</h1>
+      <form hx-post="/render" hx-target="#result">
+        <input name="markup" type="text" />
+        <div id="result" />
+        <button>Submit</button>
+      </form>
     </BaseHtml>
   );
 });
 
-let chiefsHaveBall = true;
-let bills = 0;
-let chiefs = 0;
-
-function getPoints() {
-  const number = Math.floor(Math.random() * 10);
-  const touchdown = 7;
-  const fieldGoal = 3;
-  return number >= 8 ? touchdown : number >= 5 ? fieldGoal : 0;
-}
-
-app.get("/score", async (c: Context) => {
-  if (chiefsHaveBall) {
-    chiefs += getPoints();
-  } else {
-    bills += getPoints();
-  }
-  chiefsHaveBall = !chiefsHaveBall;
-
-  // Returning a status of 286 terminates fixed rate polling.
-  c.status(chiefs > 30 || bills > 30 ? 286 : 200);
-
-  return c.text(`Chiefs: ${chiefs}, Bills: ${bills}`);
+app.post('/render', async (c: Context) => {
+  const data = await c.req.formData();
+  const markup = data.get('markup');
+  console.log('index.tsx /render: markup =', markup);
+  // const markup = '<p>before</p><script>alert("pwned")</script><p>after</p>';
+  return c.html(sanitizeHtml(markup));
 });
 
 export default app;
