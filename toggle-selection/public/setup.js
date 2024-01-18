7f7f892aa7ee5626e1436105435d1740dbea74d0
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-01-18 14:51:17 -0600

    progress

---
+++ toggle-selection/public/setup.js
@@ -0,0 +1,12 @@
+// This triggers a page refresh every time the server is restarted.
+// That happens automatically when changes to index.tsx are saved.
+//
+// This needs to be a .js file, not a .ts file,
+// because it's loaded by the browser.
+
+const ws = new WebSocket('ws://localhost:3001');
+ws.addEventListener('close', event => {
+  setTimeout(() => {
+    window.location.reload();
+  }, 500); // gives the server time to restart
+});
---
+++ toggle-selection/src/index.tsx
@@ -0,0 +1,68 @@
+import {Hono} from 'hono';
+import type {Context} from 'hono';
+import {serveStatic} from 'hono/bun';
+import type {FC} from 'hono/jsx';
+import WebSocket from 'ws';
+
+const app = new Hono();
+
+// This serves static files from the public directory.
+app.use('/*', serveStatic({root: './public'}));
+
+const dogs = ['Comet', 'Maisey', 'Oscar', 'Ramsay'];
+
+const Layout: FC = ({children}) => (
+  <html lang="en">
+    <head>
+      <title>Toggle Selection</title>
+      <link rel="stylesheet" href="/styles.css" />
+      <script
+        src="https://unpkg.com/htmx.org@1.9.10"
+        integrity="sha384-D1Kt99CQMDuVetoL1lrYwg5t+9QdHe7NLX/SoJYkXDFfX37iInKRy5xLSi8nO7UC"
+        crossorigin="anonymous"
+      ></script>
+      <script defer src="setup.js"></script>
+    </head>
+    <body>{children}</body>
+  </html>
+);
+
+app.get('/', (c: Context) =>
+  c.html(
+    <Layout>
+      {dogs.map(dog => (
+        <Dog name={dog} />
+      ))}
+    </Layout>
+  )
+);
+
+let selectedName = 'Comet';
+
+type DogProps = {name: string};
+function Dog({name}: DogProps) {
+  const classes = 'name' + (name === selectedName ? ' selected' : '');
+  return (
+    <div
+      class={classes}
+      hx-get={`/toggle/${name}`}
+      hx-swap="outerHTML"
+      hx-trigger="click"
+    >
+      {name}
+    </div>
+  );
+}
+
+app.get('/toggle/:name', (c: Context) => {
+  const name = c.req.param('name');
+  selectedName = name === selectedName ? '' : name;
+  return c.html(<Dog name={name} />);
+});
+
+// The browser code connects to this
+// so it can detect when the server is restarted.
+// On restart, the browser reloads the page.
+new WebSocket.Server({port: 3001});
+
+export default app;
