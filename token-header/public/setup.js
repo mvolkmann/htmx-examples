46ccdff53d425be622ab1474a3487eb010eb61a4
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-01-18 10:50:06 -0600

    progress

--- token-header/public/setup.js
+++ token-header/public/setup.js
@@ -1,4 +1,4 @@
 document.body.addEventListener('htmx:configRequest', event => {
   console.log('setup.js : event.detail =', event.detail);
-  event.detail.headers['x-token'] = 'my-token';
+  event.detail.headers['X-Token'] = 'my-token';
 });
