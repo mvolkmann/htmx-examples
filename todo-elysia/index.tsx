ff97343010ff11b338cdedbf22e3521d8cfecd1b
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-01-18 08:54:46 -0600

    progress

--- todo-elysia/index.tsx
+++ todo-elysia/index.tsx
@@ -45,7 +45,11 @@ const Layout = ({children}: Attributes) => (
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>To Do List</title>
       <link rel="stylesheet" href="/styles.css" />
-      <script src="https://unpkg.com/htmx.org@1.9.10"></script>
+      <script
+        src="https://unpkg.com/htmx.org@1.9.10"
+        integrity="sha384-D1Kt99CQMDuVetoL1lrYwg5t+9QdHe7NLX/SoJYkXDFfX37iInKRy5xLSi8nO7UC"
+        crossorigin="anonymous"
+      ></script>
       <script defer src="setup.js"></script>
     </head>
     <body class="p-8">{children}</body>
