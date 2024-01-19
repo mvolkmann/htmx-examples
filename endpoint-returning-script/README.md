255b28f55312d98118aa5fe59ed147f1bd268e77
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-01-19 09:24:26 -0600

    progress

---
+++ endpoint-returning-script/README.md
@@ -0,0 +1,10 @@
+# endpoint-returning-script
+
+This project demonstrates an endpoint that returns HTML
+including a `script` tag that contains JavaScript code.
+
+To run this:
+
+1. Enter `bun install`
+1. Enter `bun run dev`
+1. Browse http://localhost:3000
---
+++ endpoint-returning-script/src/index.tsx
@@ -0,0 +1,26 @@
+import {Hono} from 'hono';
+import type {Context} from 'hono';
+import type {FC} from 'hono/jsx';
+
+const app = new Hono();
+
+const BaseHtml: FC = ({children}) => (
+  <html>
+    <head>
+      <title>Endpoint Returning Script</title>
+      <script src="https://unpkg.com/htmx.org@1.9.9"></script>
+      <script>console.log('Hello, World!');</script>
+    </head>
+    <body>{children}</body>
+  </html>
+);
+
+app.get('/', (c: Context) => {
+  return c.html(
+    <BaseHtml>
+      <h1>Endpoint Returning Script</h1>
+    </BaseHtml>
+  );
+});
+
+export default app;
