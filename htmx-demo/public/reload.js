7951ee8fc42a4250c657653fb8aaea8d164ed1e7
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-02-02 16:08:48 -0600

    progress

---
+++ htmx-demo/public/reload.js
@@ -0,0 +1,13 @@
+ws = new WebSocket("ws://localhost:3001");
+
+ws.addEventListener("close", (event) => {
+  // This assumes the server will restart and create a new WebSocket server.
+  setTimeout(() => {
+    window.location.reload();
+  }, 500); // gives the server time to restart
+});
+
+ws.addEventListener("message", (event) => {
+  console.log("reload.js message: event =", event);
+  if (event.data === "reload") location.reload();
+});
--- htmx-demo/src/server.tsx
+++ htmx-demo/src/server.tsx
@@ -1,5 +1,21 @@
 import { type Context, Hono } from "hono";
 import { serveStatic } from "hono/bun";
+import { watch } from "fs";
+import WebSocket from "ws";
+
+// Browser code will connect to this so it
+// can detect when the server is restarted.
+// On restart, the browser will reload the page.
+let wss = new WebSocket.Server({ port: 3001 }); // choose any unused port
+
+// If any files in or below the public directory change,
+// send the client a message to tell it to reload.
+watch("./public", { recursive: true }, (event, filename) => {
+  console.log(`detected ${event} in ${filename}`);
+  for (const client of wss.clients) {
+    client.send("reload");
+  }
+});
 
 const app = new Hono();
 
@@ -9,7 +25,7 @@ app.use("/*", serveStatic({ root: "./public" }));
 app.get("/version", async (c: Context) => {
   // Return a Response whose body contains
   // the version of Bun running on the server.
-  return c.text(Bun.version);
+  return c.text("v" + Bun.version);
 });
 
 export default app;
