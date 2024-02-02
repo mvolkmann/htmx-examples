e0075aa793996cc456fd452b5d87bbe7c89d0d64
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-02-02 11:16:13 -0600

    progress

--- first-demo/index.tsx
+++ first-demo/index.tsx
@@ -9,7 +9,7 @@ app.use("/*", serveStatic({ root: "./public" }));
 // TODO: Instead of this, use an endpoint that returns data not available in the client.
 // TODO: You can open multiple clients and let each change the value.
 app.get("/time/:count", async (c: Context) => {
-  const count = Number(c.req.param("count"));
+  const count = c.req.param("count");
   const time = new Date().toLocaleTimeString();
   return c.text(`The count at ${time} was ${count}.`);
 });
