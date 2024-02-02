6f77729a933ccf675741e494f071844403a1b370
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-02-02 09:57:57 -0600

    progress

--- first-demo/index.tsx
+++ first-demo/index.tsx
@@ -6,8 +6,12 @@ const app = new Hono();
 // Serve static files from the public directory.
 app.use("/*", serveStatic({ root: "./public" }));
 
-app.get("/time", async (c: Context) => {
-  return c.text(new Date().toLocaleTimeString());
+// TODO: Instead of this, use an endpoint that returns data not available in the client.
+// TODO: You can open multiple clients and let each change the value.
+app.get("/time/:count", async (c: Context) => {
+  const count = Number(c.req.param("count"));
+  const time = new Date().toLocaleTimeString();
+  return c.text(`The count at ${time} was ${count}.`);
 });
 
 export default app;
