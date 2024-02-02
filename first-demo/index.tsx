8f1fec112c26d529d6afd87f81d32a95e989af12
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-02-01 20:25:45 -0600

    progress

--- first-demo/index.tsx
+++ first-demo/index.tsx
@@ -1,43 +1,11 @@
 import { Hono } from "hono";
+import { serveStatic } from "hono/bun";
 import type { Context } from "hono";
-import type { FC } from "hono/jsx";
 
 const app = new Hono();
 
-const Layout: FC = ({ children }) => (
-  <html>
-    <head>
-      <title>htmx First Demo</title>
-      <script src="https://unpkg.com/htmx.org@1.9.10"></script>
-      <script
-        defer
-        src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"
-      ></script>
-    </head>
-    <body>{children}</body>
-  </html>
-);
-
-app.get("/", (c: Context) => {
-  return c.html(
-    <Layout>
-      <button hx-get="/date" hx-target="#date">
-        Get Data
-      </button>
-      <div id="date"></div>
-
-      {/* Alpine demo */}
-      <br />
-      <div style="display: flex; gap: 1rem" x-data="{ count: 0 }">
-        <button x-bind:disabled="count <= 0" x-on:click="count--">
-          -
-        </button>
-        <div x-text="count"></div>
-        <button x-on:click="count++">+</button>
-      </div>
-    </Layout>
-  );
-});
+// Serve static files from the public directory.
+app.use("/*", serveStatic({ root: "./public" }));
 
 app.get("/date", async (c: Context) => {
   return c.text(new Date().toLocaleDateString());
