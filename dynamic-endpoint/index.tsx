9e38327b26384e6766e7269a660b78ed19fac380
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-02-04 09:42:31 -0600

    progress

---
+++ dynamic-endpoint/index.tsx
@@ -0,0 +1,15 @@
+import { type Context, Hono } from "hono";
+import { serveStatic } from "hono/bun";
+
+const app = new Hono();
+
+// Serve static files from the public directory.
+app.use("/*", serveStatic({ root: "./public" }));
+
+app.get("/time/:count", async (c: Context) => {
+  const count = c.req.param("count");
+  const time = new Date().toLocaleTimeString();
+  return c.text(`The count at ${time} was ${count}.`);
+});
+
+export default app;
