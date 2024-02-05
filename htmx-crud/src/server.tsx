223a386b3ddec756ebf9310c4bddaf3bf7613db9
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-02-04 19:08:37 -0600

    progress

--- htmx-crud/src/server.tsx
+++ htmx-crud/src/server.tsx
@@ -21,6 +21,8 @@ type Dog = {id: string; name: string; breed: string};
 
 const dogs = new Map<string, Dog>();
 
+let selectedId = '';
+
 function addDog(name: string, breed: string): Dog {
   const id = crypto.randomUUID(); // standard web API
   const dog = {id, name, breed};
@@ -31,9 +33,9 @@ function addDog(name: string, breed: string): Dog {
 addDog('Comet', 'Whippet');
 addDog('Oscar', 'German Shorthaired Pointer');
 
-function dogRow(dog: Dog, swap = false) {
+function dogRow(dog: Dog, updating = false) {
   const attrs: {[key: string]: string} = {};
-  if (swap) {
+  if (updating) {
     attrs['hx-swap-oob'] = 'true';
     attrs['x-init'] = 'selectedDog = null';
   }
@@ -48,12 +50,17 @@ function dogRow(dog: Dog, swap = false) {
           hx-delete={`/dog/${dog.id}`}
           hx-swap="outerHTML"
           hx-target="closest tr"
+          type="button"
         >
           ✕
         </button>
+        {/* This selects the dog which triggers a selection-change event
+            which causes the form to update. */}
         <button
           class="show-on-hover"
-          x-on:click={`selectedDog = ${JSON.stringify(dog)}`}
+          hx-get={'/select/' + dog.id}
+          hx-swap="none"
+          type="button"
         >
           ✎
         </button>
@@ -67,37 +74,112 @@ const app = new Hono();
 // Serve static files from the public directory.
 app.use('/*', serveStatic({root: './public'}));
 
-app.get('/dog', (c: Context) => {
+// Deselects the currently selected dog.
+app.get('/deselect', (c: Context) => {
+  selectedId = '';
+  c.header('HX-Trigger', 'selection-change');
+  return c.text('');
+});
+
+// Deletes the dog with a given id.
+app.delete('/dog/:id', (c: Context) => {
+  const id = c.req.param('id');
+  dogs.delete(id);
+  return c.text('');
+});
+
+// Gets the proper for for either adding or updating a dog.
+app.get('/form', (c: Context) => {
+  console.log('GET /dog-form: selectedId =', selectedId);
+  const selectedDog = dogs.get(selectedId);
+  console.log('GET /dog-form: selectedDog =', selectedDog);
+
+  const attrs: {[key: string]: string} = {
+    'hx-on:htmx:after-request': 'this.reset()'
+  };
+  if (selectedId) {
+    attrs['hx-put'] = '/dog/' + selectedId;
+    // Replace an existing row.
+    attrs['hx-target'] = '#row-' + selectedId;
+    attrs['hx-swap'] = 'outerHTML';
+  } else {
+    // Add a new row.
+    attrs['hx-post'] = '/dog';
+    attrs['hx-target'] = 'table tbody';
+    attrs['hx-swap'] = 'afterbegin';
+  }
+  console.log('GET /dog-form: attrs =', attrs);
+
+  return c.html(
+    <form hx-disabled-elt="#submit-btn" {...attrs}>
+      <div>
+        <label for="name">Name</label>
+        <input
+          name="name"
+          required
+          size={30}
+          type="text"
+          value={selectedDog?.name ?? ''}
+        />
+      </div>
+      <div>
+        <label for="breed">Breed</label>
+        <input
+          name="breed"
+          required
+          size={30}
+          type="text"
+          value={selectedDog?.breed ?? ''}
+        />
+      </div>
+
+      <button id="submit-btn">{selectedId ? 'Update' : 'Add'}</button>
+      {selectedId && (
+        <button hx-get="/deselect" type="button">
+          Cancel
+        </button>
+      )}
+    </form>
+  );
+});
+
+// Gets table rows for all the dogs.
+app.get('/rows', (c: Context) => {
   const sortedDogs = Array.from(dogs.values()).sort((a, b) =>
     a.name.localeCompare(b.name)
   );
   return c.html(<>{sortedDogs.map(dog => dogRow(dog))}</>);
 });
 
+// Selects a dog.
+app.get('/select/:id', (c: Context) => {
+  selectedId = c.req.param('id');
+  c.header('HX-Trigger', 'selection-change');
+  return c.text('');
+});
+
+// Creates a dog.
 app.post('/dog', async (c: Context) => {
   const formData = await c.req.formData();
   const name = (formData.get('name') as string) || '';
   const breed = (formData.get('breed') as string) || '';
   const dog = addDog(name, breed);
+  console.log('server.tsx post: dog =', dog);
   return c.html(dogRow(dog), 201);
 });
 
-// TODO: This really should use put instead of post.
-app.post('/dog/:id', async (c: Context) => {
+// Updates a dog
+app.put('/dog/:id', async (c: Context) => {
   const id = c.req.param('id');
   const formData = await c.req.formData();
   const name = (formData.get('name') as string) || '';
   const breed = (formData.get('breed') as string) || '';
   const updatedDog = {id, name, breed};
   dogs.set(id, updatedDog);
-  return c.html(dogRow(updatedDog, true), 201);
-  // TODO: Set Alpine selectedDog back to null.
-});
 
-app.delete('/dog/:id', (c: Context) => {
-  const id = c.req.param('id');
-  dogs.delete(id);
-  return c.html('');
+  selectedId = '';
+  c.header('HX-Trigger', 'selection-change');
+  return c.html(dogRow(updatedDog, true));
 });
 
 export default app;
