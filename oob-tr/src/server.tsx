2aac2b9d98136fdb2e3d2d5406174c6d06ff168c
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-02-04 20:37:28 -0600

    progress

---
+++ oob-tr/src/server.tsx
@@ -0,0 +1,34 @@
+import {type Context, Hono} from 'hono';
+import {serveStatic} from 'hono/bun';
+import {watch} from 'fs';
+import WebSocket from 'ws';
+
+// Browser code will connect to this so it
+// can detect when the server is restarted.
+// On restart, the browser will reload the page.
+let wss = new WebSocket.Server({port: 3001}); // choose any unused port
+
+// If any files in or below the public directory change,
+// send the client a message to tell it to reload.
+watch('./public', {recursive: true}, (event, filename) => {
+  console.log(`detected ${event} in ${filename}`);
+  for (const client of wss.clients) {
+    client.send('reload');
+  }
+});
+
+const app = new Hono();
+
+// Serve static files from the public directory.
+app.use('/*', serveStatic({root: './public'}));
+
+app.get('/swap', async (c: Context) => {
+  return c.html(
+    <tr id="dog" hx-swap-oob="true">
+      <td>Fireball</td>
+      <td>Whipster</td>
+    </tr>
+  );
+});
+
+export default app;
