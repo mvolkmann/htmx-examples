3c1c7ccbaaf8d16f69811602996e19247a917533
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-02-07 10:03:15 -0600

    progress

--- htmx-crud/public/reload-client.js
+++ htmx-crud/public/reload-client.js
@@ -1,12 +1,12 @@
-ws = new WebSocket("ws://localhost:3001");
+const ws = new WebSocket('ws://localhost:3001');
 
-ws.addEventListener("close", (event) => {
+ws.addEventListener('close', event => {
   // This assumes the server will restart and create a new WebSocket server.
   setTimeout(() => {
     window.location.reload();
   }, 500); // gives the server time to restart
 });
 
-ws.addEventListener("message", (event) => {
-  if (event.data === "reload") location.reload();
+ws.addEventListener('message', event => {
+  if (event.data === 'reload') location.reload();
 });
