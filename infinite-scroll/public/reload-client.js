0c8a8309cc4598d6e43b871775c3d0a902d4eb8f
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-02-21 11:12:14 -0600

    progress

---
+++ infinite-scroll/public/reload-client.js
@@ -0,0 +1,13 @@
+// The port here must match the port used in src/reload-server.js.
+const ws = new WebSocket('ws://localhost:3001');
+
+ws.addEventListener('close', event => {
+  // This assumes the server will restart and create a new WebSocket server.
+  setTimeout(() => {
+    window.location.reload();
+  }, 500); // gives the server time to restart
+});
+
+ws.addEventListener('message', event => {
+  if (event.data === 'reload') location.reload();
+});
