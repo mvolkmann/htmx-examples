5ebb8ec0925e2ee82a8c3541064c2e762dcfa3ce
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-02-21 11:07:50 -0600

    progress

---
+++ active-search/src/server.tsx
@@ -0,0 +1,38 @@
+import {Context, Hono} from 'hono';
+import {serveStatic} from 'hono/bun';
+import './reload-server.js';
+
+const app = new Hono();
+
+// Serve static files from the public directory.
+app.use('/*', serveStatic({root: './public'}));
+
+const names: string[] = [
+  'Amanda',
+  'Gerri',
+  'Jeremy',
+  'Mark',
+  'Meghan',
+  'Pat',
+  'RC',
+  'Richard',
+  'Tami'
+];
+
+app.post('/search', async (c: Context) => {
+  const data = await c.req.formData();
+  const name = (data.get('name') as string) || '';
+  if (name == '') return c.html('');
+
+  const lowerName = name.toLowerCase();
+  const matches = names.filter(n => n.toLowerCase().includes(lowerName));
+  return c.html(
+    <>
+      {matches.map(name => (
+        <li>{name}</li>
+      ))}
+    </>
+  );
+});
+
+export default app;
