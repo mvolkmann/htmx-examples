516b75f75b97b37286bb122b0ab3d8a9239f891a
Mark Volkmann <r.mark.volkmann@gmail.com>
2024-03-16 18:15:18 -0500

    progress

--- htmx.ajax/src/server.tsx
+++ htmx.ajax/src/server.tsx
@@ -8,21 +8,6 @@ const app = new Hono();
 // Serve static files from the public directory.
 app.use('/*', serveStatic({root: './public'}));
 
-app.get('/danger', (c: Context) => {
-  /*
-  When Hono converts JSX to a string, it is escaped.
-  That causes a syntax error when the browser tries to
-  execute the script below because the quotes are replaced.
-  We can bypass the escaping by creating the HTML as a string.
-  */
-  const html = `
-    <div>First</div>
-    <script>alert('possible hack')</script>
-    <div>Second</div>
-  `;
-  return c.html(html);
-});
-
 app.get('/heap-size', (c: Context) => {
   const stats = heapStats();
   return c.text((stats.heapSize / 1024 / 1024).toFixed(2));
