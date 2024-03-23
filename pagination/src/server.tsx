abf81bc49d808ab2361a0b136f1614fb70e93eab
Mark Volkmann <r.mark.volkmann@gmail.com>
2024-03-22 19:29:37 -0500

    progress

--- pagination/src/server.tsx
+++ pagination/src/server.tsx
@@ -1,6 +1,6 @@
-import {Elysia} from 'elysia';
-import {html} from '@elysiajs/html';
-import {staticPlugin} from '@elysiajs/static';
+import {type Context, Hono} from 'hono';
+import {serveStatic} from 'hono/bun';
+import './reload-server.ts';
 
 const POKEMON_URL_PREFIX = 'https://pokeapi.co/api/v2/pokemon-species';
 const ROWS_PER_PAGE = 5;
@@ -10,15 +10,10 @@ type Pokemon = {
   url: string;
 };
 
-const app = new Elysia();
-app.use(html()); // enables use of JSX
-app.use(staticPlugin({prefix: ''})); // looks in public directory
-
 function TableRow(page: number, pokemon: Pokemon, isLast: boolean) {
   const {name, url} = pokemon;
   const id = url.split('/')[6];
   const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
-
   return (
     <tr>
       <td>{id}</td>
@@ -30,30 +25,24 @@ function TableRow(page: number, pokemon: Pokemon, isLast: boolean) {
   );
 }
 
-// Elysia does not serve index.html by default.
-// Instead, the following error will be displayed:
-// "MacOS does not support sending non-regular files"
-// This redirect fixes it.
-app.get('/', ({set}) => {
-  set.redirect = '/index.html';
-});
+const app = new Hono();
 
-app.get('/pokemon-rows', async ({headers, query}) => {
-  console.log('index.tsx : headers =', headers);
-  const {page} = query;
+// Serve static files from the public directory.
+app.use('/*', serveStatic({root: './public'}));
+
+app.get('/pokemon-rows', async (c: Context) => {
+  const page = Number(c.req.query('page'));
   if (!page) throw new Error('page query parameter is required');
 
   Bun.sleepSync(500); // simulates long-running query
 
-  const whichPage = headers['hx-prompt'];
-  const pageNumber = Number(whichPage ? whichPage : page);
-  const offset = (pageNumber - 1) * ROWS_PER_PAGE;
+  const offset = (page - 1) * ROWS_PER_PAGE;
   const url = POKEMON_URL_PREFIX + `?offset=${offset}&limit=${ROWS_PER_PAGE}`;
   const response = await fetch(url);
   const json = await response.json();
   const pokemonList = json.results as Pokemon[];
 
-  return (
+  return c.html(
     <>
       <table id="pokemon-table">
         <tr>
@@ -63,7 +52,7 @@ app.get('/pokemon-rows', async ({headers, query}) => {
         </tr>
         {pokemonList.map((pokemon, index) => {
           const isLast = index === ROWS_PER_PAGE - 1;
-          return TableRow(pageNumber, pokemon, isLast);
+          return TableRow(page, pokemon, isLast);
         })}
       </table>
 
@@ -75,22 +64,13 @@ app.get('/pokemon-rows', async ({headers, query}) => {
         hx-indicator=".htmx-indicator"
         hx-target="#pokemon-table"
       >
-        <button
-          disabled={pageNumber === 1}
-          hx-get={`/pokemon-rows?page=${pageNumber - 1}`}
-        >
+        <button disabled={page === 1} hx-get={`/pokemon-rows?page=${page - 1}`}>
           Previous
         </button>
-        <button
-          hx-get={`/pokemon-rows?page=${pageNumber + 1}`}
-          hx-prompt="Which page?"
-        >
-          Next
-        </button>
+        <button hx-get={`/pokemon-rows?page=${page + 1}`}>Next</button>
       </span>
     </>
   );
 });
 
-app.listen(1919);
-console.log('listening on port', app.server?.port);
+export default app;
