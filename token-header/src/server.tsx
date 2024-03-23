5735ee935417ba3d44affa62b582c64dbbd902a1
Mark Volkmann <r.mark.volkmann@gmail.com>
2024-03-23 13:06:29 -0500

    progress

---
+++ token-header/src/server.tsx
@@ -0,0 +1,20 @@
+import {type Context, Hono} from 'hono';
+import {serveStatic} from 'hono/bun';
+import './reload-server.ts';
+
+const app = new Hono();
+
+// This serves static files from the public directory.
+app.use('/*', serveStatic({root: './public'}));
+
+app.get('/request1', (c: Context) => {
+  const token = c.req.header('x-token');
+  return c.text(`/request1 received the token "${token}".`);
+});
+
+app.post('/request2', (c: Context) => {
+  const token = c.req.header('x-token');
+  return c.text(`/request2 received the token "${token}".`);
+});
+
+export default app;
