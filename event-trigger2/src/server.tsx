ecc23134f4c262b8122b853d721f984530ce960a
Mark Volkmann <r.mark.volkmann@gmail.com>
2024-03-29 16:33:01 -0500

    progress

--- event-trigger2/src/server.tsx
+++ event-trigger2/src/server.tsx
@@ -9,19 +9,19 @@ app.use('/*', serveStatic({root: './public'}));
 
 app.get('/event-with-no-data', (c: Context) => {
   c.header('HX-Trigger', 'event1');
-  return c.html('Event w/ no data');
+  return c.text('dispatched event1');
 });
 
 app.get('/event-with-string', (c: Context) => {
   const trigger = {event2: 'some string'};
   c.header('HX-Trigger', JSON.stringify(trigger));
-  return c.html('Event w/ string data');
+  return c.text('dispatched event2');
 });
 
 app.get('/event-with-object', (c: Context) => {
   const trigger = {event3: {foo: 1, bar: 2}};
   c.header('HX-Trigger', JSON.stringify(trigger));
-  return c.html('Event w/ object data');
+  return c.text('dispatched event3');
 });
 
 export default app;
