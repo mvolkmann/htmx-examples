e78c65a9c22e604cf6c13e234c2942bb4373cdb6
Mark Volkmann <r.mark.volkmann@gmail.com>
2024-04-05 20:07:49 -0500

    progress

--- custom-confirm/src/server.tsx
+++ custom-confirm/src/server.tsx
@@ -1,7 +1,7 @@
 import {Context, Hono} from 'hono';
 import {serveStatic} from 'hono/bun';
 
-const POKEMON_URL_PREFIX = 'https://pokeapi.co/api/v2/pokemon-species';
+const POKEMON_URL = 'https://pokeapi.co/api/v2/pokemon-species';
 
 type Pokemon = {
   name: string;
@@ -19,8 +19,7 @@ app.use('/*', serveStatic({root: './public'}));
 
 app.get('/pokemon', async (c: Context) => {
   await Bun.sleep(1000); // simulates a long-running request
-  const url = POKEMON_URL_PREFIX;
-  const response = await fetch(url);
+  const response = await fetch(POKEMON_URL);
   const json = (await response.json()) as PokemonData;
   const pokemonList = json.results as Pokemon[];
 
