a278ee376ef899c195f50989b149c2f952c9d485
Mark Volkmann <r.mark.volkmann@gmail.com>
2024-04-07 09:03:14 -0500

    progress

--- boost/src/server.tsx
+++ boost/src/server.tsx
@@ -1,10 +1,6 @@
-import {type Context, Hono} from 'hono';
+import {Hono} from 'hono';
 import {serveStatic} from 'hono/bun';
-import './reload-server';
 
 const app = new Hono();
-
-// Serve static files from the public directory.
 app.use('/*', serveStatic({root: './public'}));
-
 export default app;
