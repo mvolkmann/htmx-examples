e9149d44ee76d3ce29ef78fd5029c4521877773b
Mark Volkmann <r.mark.volkmann@gmail.com>
2024-05-07 19:25:03 -0500

    improved infinite-scroll-images and pagination-images

--- infinite-scroll-images/src/server.tsx
+++ infinite-scroll-images/src/server.tsx
@@ -1,10 +1,13 @@
+import {Glob} from 'bun';
 import {Context, Hono} from 'hono';
 import {serveStatic} from 'hono/bun';
-import {readdir} from 'node:fs/promises';
 import './reload-server';
 
-const IMAGE_DIR = './public/images';
 const ROWS_PER_PAGE = 10;
+
+const glob = new Glob('*');
+const allFilenames = [...glob.scanSync('./public/images')];
+allFilenames.sort();
 
 async function ImageRow(page: number, filename: string, isLast: boolean) {
   const attrs = isLast
@@ -15,24 +18,11 @@ async function ImageRow(page: number, filename: string, isLast: boolean) {
         'hx-swap': 'afterend'
       }
     : {};
-  const file = Bun.file(IMAGE_DIR + '/' + filename);
-  const isSVG = filename.endsWith('.svg');
-  let contents, url;
-  if (isSVG) {
-    contents = await file.text();
-  } else {
-    const arrBuf = await file.arrayBuffer();
-    url = 'data:image/png;base64,' + Buffer.from(arrBuf).toString('base64');
-  }
   return (
     <tr {...attrs}>
       <td>{filename}</td>
       <td>
-        {isSVG ? (
-          <div dangerouslySetInnerHTML={{__html: contents}} />
-        ) : (
-          <img alt={filename} src={url} />
-        )}
+        <img alt={filename} src={'./images/' + filename} />
       </td>
     </tr>
   );
@@ -43,23 +33,9 @@ const app = new Hono();
 // Serve static files from the public directory.
 app.use('/*', serveStatic({root: './public'}));
 
-let allFilenames: string[] = [];
-let imageExtensions = ['.gif', '.jpg', '.jpeg', '.png'];
-
 app.get('/image-rows', async (c: Context) => {
   const page = Number(c.req.query('page'));
   if (!page) throw new Error('page query parameter is required');
-
-  // Bun.sleepSync(500); // simulates long-running query
-
-  if (page === 1) {
-    allFilenames = await readdir(IMAGE_DIR);
-    allFilenames = allFilenames.filter(filename => {
-      const file = Bun.file(filename);
-      return file.type.startsWith('image');
-    });
-    allFilenames.sort();
-  }
 
   const offset = (page - 1) * ROWS_PER_PAGE;
   const pageFilenames = allFilenames.slice(offset, offset + ROWS_PER_PAGE);
--- pagination-images/src/server.tsx
+++ pagination-images/src/server.tsx
@@ -1,30 +1,20 @@
+import {Glob} from 'bun';
 import {type Context, Hono} from 'hono';
 import {serveStatic} from 'hono/bun';
-import {readdir} from 'node:fs/promises';
 import './reload-server';
 
-const IMAGE_DIR = './public/images';
 const ROWS_PER_PAGE = 5;
 
+const glob = new Glob('*');
+const allFilenames = [...glob.scanSync('./public/images')];
+allFilenames.sort();
+
 async function ImageRow(filename: string, isLast: boolean) {
-  const file = Bun.file(IMAGE_DIR + '/' + filename);
-  const isSVG = filename.endsWith('.svg');
-  let contents, url;
-  if (isSVG) {
-    contents = await file.text();
-  } else {
-    const arrBuf = await file.arrayBuffer();
-    url = 'data:image/png;base64,' + Buffer.from(arrBuf).toString('base64');
-  }
   return (
     <tr>
       <td>{filename}</td>
       <td>
-        {isSVG ? (
-          <div dangerouslySetInnerHTML={{__html: contents}} />
-        ) : (
-          <img alt={filename} src={url} />
-        )}
+        <img alt={filename} src={'./images/' + filename} />
       </td>
     </tr>
   );
@@ -35,23 +25,11 @@ const app = new Hono();
 // Serve static files from the public directory.
 app.use('/*', serveStatic({root: './public'}));
 
-let allFilenames: string[] = [];
-let imageExtensions = ['.gif', '.jpg', '.jpeg', '.png'];
-
 app.get('/image-rows', async (c: Context) => {
   const page = Number(c.req.query('page'));
   if (!page) throw new Error('page query parameter is required');
 
   Bun.sleepSync(500); // simulates long-running query
-
-  if (page === 1) {
-    allFilenames = await readdir(IMAGE_DIR);
-    allFilenames = allFilenames.filter(filename => {
-      const file = Bun.file(filename);
-      return file.type.startsWith('image');
-    });
-    allFilenames.sort();
-  }
 
   const offset = (page - 1) * ROWS_PER_PAGE;
   const pageFilenames = allFilenames.slice(offset, offset + ROWS_PER_PAGE);
