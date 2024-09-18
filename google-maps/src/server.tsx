6171617e96113e3e77a6385e836fe8e90b9c8f5e
Mark Volkmann <r.mark.volkmann@gmail.com>
2024-09-18 13:00:16 -0500

    progress

--- google-maps/src/server.tsx
+++ google-maps/src/server.tsx
@@ -1,8 +1,5 @@
 import {type Context, Hono} from 'hono';
 import {serveStatic} from 'hono/bun';
-// import {HtmlValidate} from 'html-validate';
-// import tagMap from 'js2htmlstr';
-// const {img} = tagMap;
 import './reload-server';
 
 const app = new Hono();
@@ -10,38 +7,6 @@ const app = new Hono();
 // Serve static files from the public directory.
 app.use('/*', serveStatic({root: './public'}));
 
-app.get('/version', (c: Context) => {
-  // Return a Response whose body contains
-  // the version of Bun running on the server.
-  return c.text('v' + Bun.version);
-
-  /*
-  // This demonstrates using JSX to generate HTML.
-  return c.html(
-    <img
-      alt="htmx logo"
-      src="https://mvolkmann.github.io/blog/assets/htmx-logo.png"
-    />
-  );
-  */
-
-  /*
-  // This demonstrates using js2htmlstr to generate HTML.
-  const html = img({
-    alt: 'htmx logo',
-    src: 'https://mvolkmann.github.io/blog/assets/htmx-logo.png'
-  });
-  // const html = '<input type="bad">';
-  const htmlValidate = new HtmlValidate();
-  const report = await htmlValidate.validateString(html);
-  if (report.valid) {
-    return c.html(html);
-  } else {
-    console.log('report =', report);
-    const message = report.results[0].messages[0].message;
-    return c.text('error: ' + message);
-  }
-  */
-});
+app.get('/api-key', (c: Context) => c.text(process.env.API_KEY ?? ''));
 
 export default app;
